dbbef920deb16fcea9428163c68660b8176835b0
duajaradat <duajaradat164@gmail.com>
2021-08-29 15:51:09 +0300

    search page front end part1

--- src/main-components/App.js
+++ src/main-components/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import './App.css';
-import Main from './Main';
 import Header from '../header-components/Header';
+import Main from './Main';
 import Footer from '../footer-components/Footer';
 import {
   BrowserRouter as Router,
+  Switch,
+  Route
 } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile"
+// import MyFavorite from "./MyFavorite"
+import { withAuth0 } from '@auth0/auth0-react';
 
-function App() {
-  return (
-      <Router>
-        <Header />
-        <Main />
-        <Footer />
-      </Router>
-  );
+
+class App extends React.Component {
+
+  render() {
+    console.log('app', this.props);
+    return (
+      <>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" >
+              {this.props.auth0.isAuthenticated ? <Main /> : <Login />}
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+          <Footer />
+
+        </Router>
+      </>
+    );
+  }
 }
 
-export default App;
+export default withAuth0(App);
--- src/main-components/Main.js
+++ src/main-components/Main.js
@@ -1,35 +1,136 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
-import Login from './Login';
-import Profile from './Profile';
-import BestBooks from './BestBooks'
+import { Form, Button } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import Login from './Login';
+// import Profile from './Profile';
+// import BestBooks from './MyFavorite'
 
 
-import {
-    Switch,
-    Route
-  } from "react-router-dom";
+// import {
+//     Switch,
+//     Route
+// } from "react-router-dom";
 
 class Main extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchCity: null
+        };
+    }
+
+    handleInput = (e) => {
+        const { isAuthenticated } = this.props.auth0;
+        e.preventDefault();
+        this.setState({
+            searchCity: e.target.value,
+        }, () => console.log(searchCity));
+    };
+
+    // getLocation = async (e) => {
+    //     e.preventDefault();
+    //     let dataRequest = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${this.state.searchCity}&format=json`;
+    //     console.log(dataRequest);
+    //     try {
+    //         let locationData = await axios.get(dataRequest);
+    //         this.setState({
+    //             cityData: locationData.data[0],
+    //             displayData: true,
+    //         })
+
+    //     } catch (error) {
+    //         console.log("getLocation");
+    //         this.setState({
+    //             errorMsg: true,
+    //             displayData: false,
+    //             displayMap: false,
+    //             cityData: {},
+
+    //         });
+    //     }
+    //     this.getMap();
+    //     this.forecast();
+    //     this.getMovie();
+
+    // };
+
     render() {
-        const {  isAuthenticated  } = this.props.auth0;
 
         return (
-            <div>
-                <Switch>
-                    <Route exact path="/">
-                        {isAuthenticated ? <BestBooks /> : <Login />}
-                    </Route>
-                    <Route exact path="/profile">
+            <>
+                <Form className="form" onSubmit={this.handleInput}>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Control
+                            size="lg"
+                            type="text"
+                            placeholder="Enter City"
+                        />
+                        <Button
+                            className="button"
+                            variant="primary"
+                            type="submit"
+                        >
+                            Explore!
+                        </Button>
+                    </Form.Group>
+                </Form>
+                {/* <Card className="card" style={{ width: "18rem" }}>
+                    {this.state.displayMap ? <Card.Img src={this.state.imgData} /> : ""}
+                    {this.state.displayData ? (
+                        <Card.Header>{this.state.cityData.display_name}</Card.Header>
+                    ) : (
+                        ""
+                    )}
+                    {this.state.displayData ? (
+                        <Card.Body>
+                            {" "}
+                            Latitude : {this.state.cityData.lat} <br /> Longitude :{" "}
+                            {this.state.cityData.lon}{" "}
+                        </Card.Body>
+                    ) : (
+                        ""
+                    )}
+                </Card>
 
-                        <Profile />
-                    </Route>
-                </Switch>
+                {this.state.displayforecast ? <Card>
+                    <Card.Header>WEATHER</Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            {this.state.forecastData ? <Weather info={this.state.forecastData} /> : ""}
+                        </Card.Text>
+                    </Card.Body>
+                </Card> : ""}
 
-            </div>
+
+                {this.state.displayMovie ? <Card>
+                    <Card.Header>MOVIES</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Movies here</Card.Title>
+                        <Card.Text>
+                            {this.state.movieData ? <Movies info={this.state.movieData} /> : ""}
+                        </Card.Text>
+                    </Card.Body>
+                </Card> : ""}
+
+                {this.state.errorMsg ? (
+                    <Alert variant="danger" dismissible >
+                        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                        <p>
+                            Data does not exists.
+                        </p>
+                    </Alert>
+                ) : (
+                    ""
+                )}
+            </>
         );
+
+    } */}
+            </>
+        )
     }
 }
+export default withAuth0(Main);
 
-export default  withAuth0(Main);
