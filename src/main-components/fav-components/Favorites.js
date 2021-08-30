a1115f6fc980aa4386ad4d2f66766c085cc89c62
tradalhariri <trad.alhariri@gmail.com>
2021-08-30 12:52:05 +0300

    refactoring the code and solve the submit form issue

--- src/main-components/fav-components/Favorites.js
+++ src/main-components/fav-components/Favorites.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Favorites.css';
@@ -10,93 +9,147 @@ import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
-class Favorites extends React.Component{
+class Favorites extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-          restaurants:[]
-        }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      restaurants: this.props.cityData.restaurants,
+      location: this.props.cityData.cityLocation
     }
 
-
-    getRestaurantsData = async ()=>{
-      // const rootPath = constants.rootPath;
-    let restaurants = await  axios.get(`${[process.env.REACT_APP_SERVER]}/restaurants`);
-    restaurants.data.length > 0 ? this.setState({
-      restaurants: restaurants.data
-    }) : this.setState({
-      restaurants: []
-    })
+    console.log(this.state,'state');
+  }
 
 
-      console.log('nnnnnnntg',this.state.restaurants);
-    
-    }
+  // getRestaurantsData = async ()=>{
+  //   // const rootPath = constants.rootPath;
+  // let restaurants = await  axios.get(`${[process.env.REACT_APP_SERVER]}/restaurants`);
+  // restaurants.data.length > 0 ? this.setState({
+  //   restaurants: restaurants.data
+  // }) : this.setState({
+  //   restaurants: []
+  // })
 
-    
-    async componentDidMount () {
+
+  //   console.log('nnnnnnntg',this.state.restaurants);
+
+  // }
+
+
+  async componentDidMount() {
+    // console.log('state for map', this.state);
 
     // Creates new map instance
+    // const map = new mapboxgl.Map({
+    //   container: this.mapWrapper,
+    //   style: 'mapbox://styles/mapbox/streets-v10',
+    //   center: [this.state.location.lon, this.state.location.lat],
+    //   zoom: 12
+    // });
     const map = new mapboxgl.Map({
       container: this.mapWrapper,
       style: 'mapbox://styles/mapbox/streets-v10',
-      center: [ -0.118092, 51.509865],
+      center: [this.props.cityData.cityLocation.lon,this.props.cityData.cityLocation.lat],
       zoom: 12
     });
-
+    console.log(this.map,"wwwww");
     // Creates new directions control instance
     const directions = new MapboxDirections({
       accessToken: mapboxgl.accessToken,
       unit: 'metric',
       profile: 'mapbox/driving',
     });
-    await this.getRestaurantsData();
+    // await this.getRestaurantsData();
 
-    this.state.restaurants.map(restaurant => new mapboxgl.Marker({
+    this.props.cityData.restaurants.map(restaurant => new mapboxgl.Marker({
       color: "#FFF555",
       draggable: true
-      }).setLngLat([restaurant.coordinates.longitude, restaurant.coordinates.latitude]).setPopup(new mapboxgl.Popup().setHTML(`<p>${restaurant.name}</p>`))
-      .addTo(map) )
+    }).setLngLat([restaurant.coordinates.longitude, restaurant.coordinates.latitude]).setPopup(new mapboxgl.Popup().setHTML(`<div>
+      <h2>
+      ${restaurant.name}
+      <h2/>
+      <img style="width:100%; height:100%" src=${restaurant.image_url} alt=${restaurant.name} title=${restaurant.name}/>
+      </div>`))
+      .addTo(map))
 
-    // const marker = new mapboxgl.Marker({
-    //   color: "#FFF555",
-    //   draggable: true
-    //   }).setLngLat([-73.985664, 40.748514]).setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
-    //   .addTo(map);
 
     // Integrates directions control with map
     map.addControl(directions, 'top-left');
     map.addControl(new mapboxgl.GeolocateControl({
       positionOptions: {
-      enableHighAccuracy: true
+        enableHighAccuracy: true
       },
       trackUserLocation: true,
       showUserHeading: true,
-      showAccuracyCircle:true
+      showAccuracyCircle: true
 
-      }));
-     
+    }));
 
 
-      console.log('first',this.state.restaurants);
+
+    // console.log('first',this.state.restaurants);
   }
 
-    render(){
+  componentDidUpdate(){
+    const map = new mapboxgl.Map({
+      container: this.mapWrapper,
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [this.props.cityData.cityLocation.lon,this.props.cityData.cityLocation.lat],
+      zoom: 12
+    });
+    console.log(this.map,"wwwww");
+    // Creates new directions control instance
+    const directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/driving',
+    });
+    // await this.getRestaurantsData();
 
-        return (
-            <>
-            <h2 style={{margin:"20px auto"}}>Restaurants </h2>
-            <div ref={el => (this.mapWrapper = el)} className="mapWrapper" />
+    this.props.cityData.restaurants.map(restaurant => new mapboxgl.Marker({
+      color: "#FFF555",
+      draggable: true
+    }).setLngLat([restaurant.coordinates.longitude, restaurant.coordinates.latitude]).setPopup(new mapboxgl.Popup().setHTML(`<div>
+      <h2>
+      ${restaurant.name}
+      <h2/>
+      <img style="width:100%; height:100%" src=${restaurant.image_url} alt=${restaurant.name} title=${restaurant.name}/>
+      </div>`))
+      .addTo(map))
 
-            {/* <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+
+    // Integrates directions control with map
+    map.addControl(directions, 'top-left');
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+      showAccuracyCircle: true
+
+    }));
+
+
+
+  }
+  render() {
+
+    return (
+      <>
+        <h2 style={{ margin: "20px auto" }}>Restaurants{this.props.cityData.restaurants[0].name} </h2>
+        <div ref={el => (this.mapWrapper = el)} className="mapWrapper" />
+
+        {/* <Row xs={1} sm={2} md={3} lg={4} className="g-4">
             {this.props.restaurants.map((restaurant, idx) => (
               <Restaurant restaurant={restaurant} key={idx} />
             ))}
           </Row> */}
-          </>
-        )
-    }
+      </>
+    )
+  }
 }
 
 export default Favorites;
